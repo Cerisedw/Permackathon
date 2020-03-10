1dc673c7b1256eb82af15b88784882742a99e9ab
Marie Cambie <cambie.marie@gmail.com>
2020-03-10 15:25:12 +0100

    added frontend folder

---
+++ Frontend/js/jsonTest.js
@@ -0,0 +1,444 @@
+let myArray = [
+    {
+        "nom": "Réparation frigo",
+        "description": "Une réparation du frigo qui fuit dans le sous-sol.",
+        "dateCreation": "2020-03-10T11:55:16.4625883+01:00",
+        "dateDeadLine": null,
+        "createur": {
+            "nom": "Pikachu",
+            "prenom": "Michael",
+            "email": "Mike@charleroi.com",
+            "img": "https://www.revivre.org/wp-content/uploads/2016/03/retablissement.png",
+            "telephone": "0477 55 55 55 ",
+            "entreprise": {
+                "nom": "PermaFungi",
+                "telephone": "02 555 55 55",
+                "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+            },
+            "poste": "Stagiaire"
+        },
+        "priorite": {
+            "nom": "Haute",
+            "niveau": 3
+        },
+        "statut": "En attente",
+        "entreprise": {
+            "nom": "PermaFungi",
+            "telephone": "02 555 55 55",
+            "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+        },
+        "type": {
+            "nom": "R&H",
+            "parent": {
+                "nom": "Production",
+                "parent": null
+            }
+        },
+        "id": "6bd13a72-e3d2-48c0-a47a-73553ed3e212",
+        "participants": [
+            {
+                "nom": "Pikachu",
+                "prenom": "Michael",
+                "email": "Mike@charleroi.com",
+                "img": "https://www.revivre.org/wp-content/uploads/2016/03/retablissement.png",
+                "telephone": "0477 55 55 55 ",
+                "entreprise": {
+                    "nom": "PermaFungi",
+                    "telephone": "02 555 55 55",
+                    "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+                },
+                "poste": "Stagiaire"
+            },
+            {
+                "nom": "Pikachu",
+                "prenom": "Michael",
+                "email": "Mike@charleroi.com",
+                "img": "https://www.revivre.org/wp-content/uploads/2016/03/retablissement.png",
+                "telephone": "0477 55 55 55 ",
+                "entreprise": {
+                    "nom": "PermaFungi",
+                    "telephone": "02 555 55 55",
+                    "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+                },
+                "poste": "Stagiaire"
+            }
+        ]
+    },
+    {
+        "nom": "Réparation frigo TEST",
+        "description": "Une réparation du frigo qui fuit dans le sous-sol.",
+        "dateCreation": "2020-03-10T11:55:16.4642218+01:00",
+        "dateDeadLine": null,
+        "createur": {
+            "nom": "Pikachu",
+            "prenom": "Michael",
+            "email": "Mike@charleroi.com",
+            "img": "https://www.revivre.org/wp-content/uploads/2016/03/retablissement.png",
+            "telephone": "0477 55 55 55 ",
+            "entreprise": {
+                "nom": "PermaFungi",
+                "telephone": "02 555 55 55",
+                "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+            },
+            "poste": "Stagiaire"
+        },
+        "priorite": {
+            "nom": "Haute",
+            "niveau": 3
+        },
+        "statut": "En attente",
+        "entreprise": {
+            "nom": "PermaFungi",
+            "telephone": "02 555 55 55",
+            "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+        },
+        "type": {
+            "nom": "R&H",
+            "parent": {
+                "nom": "Production",
+                "parent": null
+            }
+        },
+        "id": "797bd912-a2fc-4616-a323-e6cc02659ac8",
+        "participants": [
+            {
+                "nom": "Pikachu",
+                "prenom": "Michael",
+                "email": "Mike@charleroi.com",
+                "img": "https://www.revivre.org/wp-content/uploads/2016/03/retablissement.png",
+                "telephone": "0477 55 55 55 ",
+                "entreprise": {
+                    "nom": "PermaFungi",
+                    "telephone": "02 555 55 55",
+                    "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+                },
+                "poste": "Stagiaire"
+            },
+            {
+                "nom": "Pikachu",
+                "prenom": "Michael",
+                "email": "Mike@charleroi.com",
+                "img": "https://www.revivre.org/wp-content/uploads/2016/03/retablissement.png",
+                "telephone": "0477 55 55 55 ",
+                "entreprise": {
+                    "nom": "PermaFungi",
+                    "telephone": "02 555 55 55",
+                    "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+                },
+                "poste": "Stagiaire"
+            }
+        ]
+    },
+    {
+        "nom": "Réparation frigo",
+        "description": "Une réparation du frigo qui fuit dans le sous-sol.",
+        "dateCreation": "2020-03-10T11:55:16.4642254+01:00",
+        "dateDeadLine": null,
+        "createur": {
+            "nom": "Pikachu",
+            "prenom": "Michael",
+            "email": "Mike@charleroi.com",
+            "img": "https://www.revivre.org/wp-content/uploads/2016/03/retablissement.png",
+            "telephone": "0477 55 55 55 ",
+            "entreprise": {
+                "nom": "PermaFungi",
+                "telephone": "02 555 55 55",
+                "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+            },
+            "poste": "Stagiaire"
+        },
+        "priorite": {
+            "nom": "Haute",
+            "niveau": 3
+        },
+        "statut": "En attente",
+        "entreprise": {
+            "nom": "PermaFungi",
+            "telephone": "02 555 55 55",
+            "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+        },
+        "type": {
+            "nom": "Something",
+            "parent": null
+        },
+        "id": "4518fd72-9acc-4a39-88af-da192e89873b",
+        "participants": [
+            {
+                "nom": "Pikachu",
+                "prenom": "Michael",
+                "email": "Mike@charleroi.com",
+                "img": "https://www.revivre.org/wp-content/uploads/2016/03/retablissement.png",
+                "telephone": "0477 55 55 55 ",
+                "entreprise": {
+                    "nom": "PermaFungi",
+                    "telephone": "02 555 55 55",
+                    "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+                },
+                "poste": "Stagiaire"
+            },
+            {
+                "nom": "Pikachu",
+                "prenom": "Michael",
+                "email": "Mike@charleroi.com",
+                "img": "https://www.revivre.org/wp-content/uploads/2016/03/retablissement.png",
+                "telephone": "0477 55 55 55 ",
+                "entreprise": {
+                    "nom": "PermaFungi",
+                    "telephone": "02 555 55 55",
+                    "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+                },
+                "poste": "Stagiaire"
+            }
+        ]
+    },
+    {
+        "nom": "Réparation frigo",
+        "description": "Une réparation du frigo qui fuit dans le sous-sol.",
+        "dateCreation": "2020-03-10T11:55:16.4642254+01:00",
+        "dateDeadLine": null,
+        "createur": {
+            "nom": "Pikachu",
+            "prenom": "Michael",
+            "email": "Mike@charleroi.com",
+            "img": "https://www.revivre.org/wp-content/uploads/2016/03/retablissement.png",
+            "telephone": "0477 55 55 55 ",
+            "entreprise": {
+                "nom": "PermaFungi",
+                "telephone": "02 555 55 55",
+                "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+            },
+            "poste": "Stagiaire"
+        },
+        "priorite": {
+            "nom": "Haute",
+            "niveau": 3
+        },
+        "statut": "En attente",
+        "entreprise": {
+            "nom": "PermaFungi",
+            "telephone": "02 555 55 55",
+            "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+        },
+        "type": {
+            "nom": "Something",
+            "parent": null
+        },
+        "id": "4518fd72-9acc-4a39-88af-da192e89873b",
+        "participants": [
+            {
+                "nom": "Pikachu",
+                "prenom": "Michael",
+                "email": "Mike@charleroi.com",
+                "img": "https://www.revivre.org/wp-content/uploads/2016/03/retablissement.png",
+                "telephone": "0477 55 55 55 ",
+                "entreprise": {
+                    "nom": "PermaFungi",
+                    "telephone": "02 555 55 55",
+                    "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+                },
+                "poste": "Stagiaire"
+            },
+            {
+                "nom": "Pikachu",
+                "prenom": "Michael",
+                "email": "Mike@charleroi.com",
+                "img": "https://www.revivre.org/wp-content/uploads/2016/03/retablissement.png",
+                "telephone": "0477 55 55 55 ",
+                "entreprise": {
+                    "nom": "PermaFungi",
+                    "telephone": "02 555 55 55",
+                    "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+                },
+                "poste": "Stagiaire"
+            }
+        ]
+    },
+    {
+        "nom": "Réparation frigo",
+        "description": "Une réparation du frigo qui fuit dans le sous-sol.",
+        "dateCreation": "2020-03-10T11:55:16.4642254+01:00",
+        "dateDeadLine": null,
+        "createur": {
+            "nom": "Pikachu",
+            "prenom": "Michael",
+            "email": "Mike@charleroi.com",
+            "img": "https://www.revivre.org/wp-content/uploads/2016/03/retablissement.png",
+            "telephone": "0477 55 55 55 ",
+            "entreprise": {
+                "nom": "PermaFungi",
+                "telephone": "02 555 55 55",
+                "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+            },
+            "poste": "Stagiaire"
+        },
+        "priorite": {
+            "nom": "Haute",
+            "niveau": 3
+        },
+        "statut": "En cours",
+        "entreprise": {
+            "nom": "PermaFungi",
+            "telephone": "02 555 55 55",
+            "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+        },
+        "type": {
+            "nom": "Something",
+            "parent": null
+        },
+        "id": "4518fd72-9acc-4a39-88af-da192e89873b",
+        "participants": [
+            {
+                "nom": "Pikachu",
+                "prenom": "Michael",
+                "email": "Mike@charleroi.com",
+                "img": "https://www.revivre.org/wp-content/uploads/2016/03/retablissement.png",
+                "telephone": "0477 55 55 55 ",
+                "entreprise": {
+                    "nom": "PermaFungi",
+                    "telephone": "02 555 55 55",
+                    "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+                },
+                "poste": "Stagiaire"
+            },
+            {
+                "nom": "Pikachu",
+                "prenom": "Michael",
+                "email": "Mike@charleroi.com",
+                "img": "https://www.revivre.org/wp-content/uploads/2016/03/retablissement.png",
+                "telephone": "0477 55 55 55 ",
+                "entreprise": {
+                    "nom": "PermaFungi",
+                    "telephone": "02 555 55 55",
+                    "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+                },
+                "poste": "Stagiaire"
+            }
+        ]
+    },
+    {
+        "nom": "Réparation frigo",
+        "description": "Une réparation du frigo qui fuit dans le sous-sol.",
+        "dateCreation": "2020-03-10T11:55:16.4642254+01:00",
+        "dateDeadLine": null,
+        "createur": {
+            "nom": "Pikachu",
+            "prenom": "Michael",
+            "email": "Mike@charleroi.com",
+            "img": "https://www.revivre.org/wp-content/uploads/2016/03/retablissement.png",
+            "telephone": "0477 55 55 55 ",
+            "entreprise": {
+                "nom": "PermaFungi",
+                "telephone": "02 555 55 55",
+                "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+            },
+            "poste": "Stagiaire"
+        },
+        "priorite": {
+            "nom": "Haute",
+            "niveau": 3
+        },
+        "statut": "Fini",
+        "entreprise": {
+            "nom": "PermaFungi",
+            "telephone": "02 555 55 55",
+            "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+        },
+        "type": {
+            "nom": "Something",
+            "parent": null
+        },
+        "id": "4518fd72-9acc-4a39-88af-da192e89873b",
+        "participants": [
+            {
+                "nom": "Pikachu",
+                "prenom": "Michael",
+                "email": "Mike@charleroi.com",
+                "img": "https://www.revivre.org/wp-content/uploads/2016/03/retablissement.png",
+                "telephone": "0477 55 55 55 ",
+                "entreprise": {
+                    "nom": "PermaFungi",
+                    "telephone": "02 555 55 55",
+                    "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+                },
+                "poste": "Stagiaire"
+            },
+            {
+                "nom": "Pikachu",
+                "prenom": "Michael",
+                "email": "Mike@charleroi.com",
+                "img": "https://www.revivre.org/wp-content/uploads/2016/03/retablissement.png",
+                "telephone": "0477 55 55 55 ",
+                "entreprise": {
+                    "nom": "PermaFungi",
+                    "telephone": "02 555 55 55",
+                    "description": "Permafungi est une coopérative à finalité sociale basée à Bruxelles qui recycle un déchet urbain - le marc de café !"
+                },
+                "poste": "Stagiaire"
+            }
+        ]
+    }
+]
+
+
+
+// création card tache 
+
+let toDo = document.querySelector("#todo");
+let doing = document.querySelector("#doing");
+let done = document.querySelector("#done");
+
+for (let i = 0; i < myArray.length; i++ ) {
+
+    // récupération des différentes données à implanter ----------------------
+
+    let titreTache      = myArray[i].nom;
+    let nomEntreprise   = myArray[i].entreprise.nom;
+    let myId = myArray[i].id;
+
+    // création des éléments pour l'html. Ils sont placés dans la boucle pour se multiplier 20x -------------------------------------------
+
+
+
+    
+
+    let item              = document.createElement('div');
+   
+    item.className='card task-item';
+
+    let cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+
+    let name        = document.createElement('h2');
+    name.className='card-title';
+
+
+
+    let btnParticipant = document.createElement('div');
+    btnParticipant.setAttribute('data-index', myId);
+    btnParticipant.innerHTML="<br><span class='badge badge-primary'><i class='fas fa-plus text-white'></i></span>";
+    btnParticipant.addEventListener('click', (event)=>{console.log(event.target.parentNode.dataset.index)});
+
+
+    // implantation des différents éléments créés (pour le moment "flottés") dans l'html. De nouveau dans la boucle 20x.-----------------------------
+
+
+    
+
+    cardBody.textContent = titreTache + ' ' + nomEntreprise;
+
+    item.appendChild(cardBody);
+    console.log(doing);
+    
+
+    if (myArray[i].statut === 'En attente'){
+        cardBody.appendChild(btnParticipant);
+        toDo.appendChild(item);
+    } 
+    if (myArray[i].statut === 'En cours'){
+        doing.appendChild(item);
+    } 
+    if (myArray[i].statut === 'Fini'){
+        done.appendChild(item);
+    } 
+
+}
