2b96db89ae7bc9c61aa7459845c833ae729c8e7e
glallem <7669687+glallem@users.noreply.github.com>
2020-03-10 10:31:42 +0100

    Seconde moitié des interfaces

---
+++ Pangathon/Pangathon.DAL/Interfaces/ITache.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Pangathon.DAL.Interfaces
+{
+    public interface ITache<TIdType> : IEntity<TIdType>
+    {
+        string Nom { get; set; }
+        string Description { get; set; }
+        DateTime DateCreation { get; set; }
+        DateTime DateDeadline { get; set; }
+        IUtilisateur<Guid> Createur { get; set; }
+        IPriorite<Guid> Priorite { get; set; }
+        IStatut<Guid> Statut { get; set; }
+        IEntreprise<Guid> Entreprise { get; set; }
+        IType<Guid> Type { get; set; }
+    }
+}
